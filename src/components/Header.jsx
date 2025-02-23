7c46ba41b0e4dbe2c502274970267fd7d8e3d1f3
Manuel <hctmanuelortizdev@gmail.com>
2025-02-23 16:32:09 -0300

    Add: new name app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return ( 
         <header className="w-full max-w-4xl pt-4 pb-4 mb-5 flex justify-center items-center">
         <h1 className="text-2xl font-bold capitalize tracking-wider">
-          camping list
+          Lista del viajero
         </h1>
       </header>
     );
