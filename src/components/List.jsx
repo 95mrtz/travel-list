14a69e22f55d32c896e87e35215d0e7a41de5e0c
Manuel <hctmanuelortizdev@gmail.com>
2025-02-23 16:54:41 -0300

    Add: icon for add item button

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,3 +1,5 @@
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
 const List = ({ dataInfo, setDataInfo, show }) => {
 
 
@@ -29,8 +31,8 @@ const List = ({ dataInfo, setDataInfo, show }) => {
           <article key={items.title} className="pl-2 pr-2 flex flex-col gap-2">
             <article className="flex justify-between">
               <h2 className="text-xl font-bold">{items.title}</h2>
-              <button onClick={() => show(items.title)} className="pt-1 pb-1 pl-2 pr-2 flex justify-center items-center cursor-pointer">
-                +
+              <button onClick={() => show(items.title)} className="py-1 px-2 flex justify-center items-center cursor-pointer">
+                <AiOutlinePlusCircle />
               </button>
             </article>
             <ul className="list-inside flex flex-col items-start gap-2">
